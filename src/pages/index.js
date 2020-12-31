992ac9d51419b1f4fd5736c01b84239ce8f35653
William Oliveira de Lagos <william.lagos@icloud.com>
2020-12-31 17:57:56 +0000

    Cleared old code from main page and started a timer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,99 +4,45 @@ import { applyMiddleware, createStore } from "redux"
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
-// import Media from "react-media"
 // import { Carousel, Box, TextInput, Image, Text, Heading, Button } from "grommet"
 
 import Layout from "../components/layout"
-/*import SEO from "../components/seo"
-
-import factImage from "../images/fact.svg"
-import emailImage from "../images/email.svg"
-import passwordImage from "../images/password.svg"
-import mockupImage from "../images/mockup.png"
-import bgImage from "../images/bg.png"
-
-const Fact = ({text, number}) => (
-  <Box pad="small" direction="column">
-    <Image alignSelf="start" src={factImage} alt="Fact" />
-    <Heading color="#00CED1" margin="none" level={4}>FACT #{number}</Heading>
-    <Text>{text}</Text>
-  </Box>
-)
-
-const Register = (size) => (
-  <Box pad="small" background={{ "color": "#00CED1", "opacity": 0.2, "repeat": "no-repeat", "size": "cover" }}>
-    <Box pad="small" direction={size === "middle" ? "row" : "column"}>
-      <TextInput icon={<Image src={emailImage}/>} placeholder="E-mail" />
-      <TextInput icon={<Image src={passwordImage}/>} placeholder="Password" />
-    </Box>
-    <Box pad="small">
-      <Button primary label="Get Started" />
-    </Box>
-  </Box>
-)
-
-const IndexPage = () => (
-  <Media queries={{ small: "(max-width: 599px)" }}>
-    { matches => (
-      <Layout>
-        <Box background={!matches.small && {
-        "opacity": true,
-        "position": "right",
-        "repeat": "no-repeat",
-        "size": "contain",
-        "image": `url(${bgImage})`
-        }}>
-        <SEO title="Home" />
-        <Box direction="row">
-          <Box pad="medium">
-            <Box>
-              <Heading color="#00CED1" level={3}>jifcam</Heading>
-              <Heading margin="none"><strong>GET MORE</strong> Views</Heading>
-              <Heading margin="none"><strong>COLLAB</strong> with other creators</Heading>
-              <Heading margin="none"><strong>SHOWCASE</strong> your talent</Heading>
-              <Heading color="#8E8E8E" level={2}>with<br/> <strong>Mini Vertical Videos on Jifcam</strong></Heading>
-            </Box>
-            <Box direction="row">
-              { matches.small ? (
-                <Carousel>
-                  <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
-                  <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
-                  <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
-                </Carousel> ) : (
-                <>
-                  <Fact number={1} text="Vertical video viewing has increased by 277% in the last year"/>
-                  <Fact number={2} text="Short videos are 97% more likely to be watched entirely"/>
-                  <Fact number={3} text="Easily Get started by importing videos directly from your Youtube channel"/>
-                </>
-              )}
-            </Box>
-            { !matches.small && <Register size="middle"/>}
-          </Box>
-          <Box>
-            { !matches.small ? (
-                <Image fit="contain" src={mockupImage} />
-              ) : (
-                <Image fit="contain" src={mockupImage} />
-              )
-            }
-          </Box>
-        </Box>
-        { matches.small && <Register size="small"/> }
-        </Box>
-      </Layout>
-    )}
-  </Media>
-)*/
 
 const store = createStore(() => {}, {}, composeWithDevTools(applyMiddleware(thunk, logger)))
 
-const Page = () => (
-  <Provider store={store}>
-    <Layout>
-      <h1>Hello World!</h1>
-    </Layout>
-  </Provider>
-)
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <Layout>
+          <h1>Hello Pomodorino!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </Layout>
+      </Provider>
+    )
+  }
+}
 
 export default Page
