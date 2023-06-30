26559d73e5572c27dd95c79578b1997bfd4016c3
William Oliveira de Lagos <william.lagos@icloud.com>
2023-06-30 19:15:14 +0200

    feat: revamped eslint configuration for the project and adapted to standard ts code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react'
-import dayjs from 'dayjs';
+import { type ReactElement, useEffect, useState } from 'react'
+import dayjs from 'dayjs'
 import './App.css'
 
-function App() {
-  const [time, setTime] = useState<Date>(new Date());
+const App = (): ReactElement<any, any> => {
+  const [time, setTime] = useState<Date>(new Date())
 
   useEffect(() => {
     setInterval(() => {
-      setTime(new Date());
+      setTime(new Date())
     }, 1000)
-  }, []);
+  }, [])
 
   return (
     <>
       <h1>Pomodorino Timer</h1>
-      <p>Time is {dayjs(time).format("HH:mm:ss")}</p>
+      <p>Time is {dayjs(time).format('HH:mm:ss')}</p>
     </>
   )
 }
